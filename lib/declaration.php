eb39c8519bafef4529b65256e35b69f5f64b38a4
Omniwheelss <omniwheelss@gmail.com>
2017-11-12 22:44:35 +0530

    First commit for orta

---
+++ lib/declaration.php
@@ -0,0 +1,21 @@
+<?php
+
+
+######################################################
+#
+#		Variable Declaration Start
+#
+#######################################################
+
+
+	$Administration_Home="";
+	$Cook_Name = "";
+	$Title = "";
+	$Site_Heading = "";
+	$Footer_Link = "";
+	$Website_Name = "";
+	$Tmp_Upload = "";
+	$Img_Big_Prefix = "big_";
+	$Img_Thumb_Prefix = "thumb_";
+	$Logo_Name = "logo.jpg";
+?>
